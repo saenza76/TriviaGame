472ae343427892549547a157ff829b497f72c930
ANDRES SAENZ <SAENZA76@YAHOO.COM>
2016-08-23 19:01:31 -0500

    basic layout

---
+++ assets/javascript/app.js
@@ -0,0 +1,3 @@
+var questions = ["What was the final score of the 2005 National Championship Game?", "What are the Longhorn's all time record against Texas A&M?", "Who was the last Heisman Trophy Winner from Texas?", "How many years did Mack Brown coach at Texas for?", "What stadium is the annual Red River Rivalry game against the Oklahoma Sooners played at?"];
+var answers = [["40-37", "38-37", "41-38", "42-39"], ["76-37-5", "81-28-3", "72-40-5", "74-31-1"], ["Earl Campbell", "Vince Young", "Ricky Williams", "Colt McCoy"], ["12", "15", "18", "13"], ["AT&T Stadium", "DKR Memorial Stadium", "The Rose Bowl", "The Cotton Bowl"]];
+var counter = 25;
